f8b35ab7c8d1f5fed315d5fa74c6b2d0617c3d10
abhimotu <73917744+abhimotu@users.noreply.github.com>
2021-03-02 17:48:17 +0530

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -3,6 +3,6 @@ provider "aws" {
   region     = var.region
 }
 resource "aws_db_snapshot" "RDSSnap" {
-  db_instance_identifier = database-1
-  db_snapshot_identifier = database-2
+  db_instance_identifier = "database-1"
+  db_snapshot_identifier = "database-2"
 }
