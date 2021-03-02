a98a95cc6973d90ec098fb56e449c1cf3152b0af
abhimotu <73917744+abhimotu@users.noreply.github.com>
2021-03-02 17:39:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
      default = "ap-south-1"
 }
-variable "source_RDS" {
-  description = "RDS source identifier name"
-  type = string
-}
-variable "Snapname" {
-  description = "Snapshot name"
-  type = string
-}
+#variable "source_RDS" {
+#  description = "RDS source identifier name"
+#  type = string
+#}
+#variable "Snapname" {
+#  description = "Snapshot name"
+#  type = string
+#}
